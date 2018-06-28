e71215ef03990800b4b8b1e3e3e7b9793e2006ed
kooksee@163.com <kooksee@163.com>
2018-06-28 16:27:04 +0800

    修改vendor

---
+++ vendor/github.com/kooksee/kdb/consts/const.go
@@ -0,0 +1,20 @@
+package consts
+
+import "math"
+
+var (
+	Prefix     = []byte("px:")
+	DataPrefix = []byte("@@:")
+	PrefixBk   = []byte("pxb:")
+
+	// key字节范围
+	MINBYTE byte = 0
+	MAXBYTE byte = math.MaxUint8
+
+	StatueOk      = []byte("ok")
+	StatueDeleted = []byte("deleted")
+)
+
+func WithPrefix(name []byte) []byte {
+	return append(Prefix, name...)
+}
